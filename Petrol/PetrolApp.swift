174229fb0ca87cc95b904972a3907eb80d21db13
Ben Robinson <4210111+bsrobinson@users.noreply.github.com>
2024-09-10 07:23:27 +0100

    Initial Commit

---
+++ Petrol/PetrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetrolApp.swift
+//  Petrol
+//
+//  Created by Ben Robinson on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PetrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
